86709ab6354072e32b6364e4c87fcdc36934cc8f
Matthias <da-kaiser@der-ball-ist-rund.net>
2012-11-14 18:01:56 +0100

    works

--- d3/convert.js
+++ d3/convert.js
@@ -1,5 +1,6 @@
 
 function jSBGN_to_d3(network) {
+	var i;
 	for (i in network.nodes) {
 		n = network.nodes[i];
 		n.index = i;
@@ -12,9 +13,13 @@ function jSBGN_to_d3(network) {
 		}
 
 	function getNodeById(id) {
+		console.log(id);
+		var i;
 		for (i in network.nodes) {
-			if (network.nodes[i].id == id || network.nodes[i] == id)
+			if (network.nodes[i].id == id || network.nodes[i] == id) {
+//				console.log(i+': '+network.nodes[i].id);
 				return network.nodes[i];
+				}
 			}
 		console.error('node '+id+' not found. created.');
 		return network.nodes.append( {'index':network.nodes.length, 'x':1, 'y':1, 'fixed':false} );
@@ -23,8 +28,12 @@ function jSBGN_to_d3(network) {
 	//
 	// d3 expects edge source and targets specified as object links, rather than ids or indices
 	//
+	var i;
 	for (i in network.edges) {
 		network.edges[i].sourceNode = getNodeById(network.edges[i].source);
 		network.edges[i].targetNode = getNodeById(network.edges[i].target);
+		console.log('edge '+i+' from '+network.edges[i].sourceNode.id+' to '+network.edges[i].targetNode.id);
 		}
+
+	return network;
 	}
