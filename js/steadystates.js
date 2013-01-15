4fd1d4d93ab7f4c2f4f2810fc0d0337429a3d514
Matthias <da-kaiser@der-ball-ist-rund.net>
2013-01-15 17:26:34 +0100

    Create new steady states table

--- js/steadystates.js
+++ js/steadystates.js
@@ -1,4 +1,4 @@
-var SteadyStatesNodes = [];
+var networkInputNodes, networkOutputNodes = [];
 var encounteredStateCombinations = [];
 
 /*
@@ -6,37 +6,51 @@ var encounteredStateCombinations = [];
  */
 createSteadyStates = function() {
 
-	SteadyStatesNodes = [
+	networkInputNodes = [
 					"oxygen",
 					"glucose",
 					"glycerol",
 					"nitrogen",
-					"rapamycin",
+					"rapamycin"
+					];
+	networkOutputNodes = [
 					"autophagy",
 					"basal_mitophagy",
 					"elevated_mitophagy"
-				];
+					];
 	
-	var SteadyStates = "<tr><th colspan="+SteadyStatesNodes.length+">encountered steady-state node state combinations</th></tr>";
-	SteadyStates += "<tr>";
+//	var html = "<tr><th colspan="+(networkInputNodes.length+networkOutputNodes.length+1)+">Passed steady states</th></tr>\n";
+	var html = "<tr><th colspan="+networkInputNodes.length+">Input nodes</th>";
+	html += '<th style="width: 10px;">&nbsp;</th>';
+	html += "<th colspan="+networkOutputNodes.length+">Output nodes</th></tr>";
+	html += "<tr>\n";
 	var i;
-	for (i in SteadyStatesNodes) {
-		SteadyStates += "<td>"+SteadyStatesNodes[i]+"</td>";
+	for (i in networkInputNodes) {
+		html += "<th>"+networkInputNodes[i]+"</th>";
 	}
-	SteadyStates += "</tr>\n";
+	html += "<th></th>";
+	for (i in networkOutputNodes) {
+		html += "<th>"+networkOutputNodes[i]+"</th>";
+	}
+	html += "</tr>\n";
 
 	// set content of SteadyStates <table>
 	$("#SteadyStates")
-		.html(SteadyStates);
+		.html(html);
 }
 
-appendSteadyStatesTable = function(states) {
+appendSteadyStatesTable = function(stateCombination) {
 	var row = "<tr>";
 	var i;
-	for (i in states) {
+	for (i in stateCombination) {
+		// spacer between input and output nodes
+		if (i == networkInputNodes.length)
+			row += '<td style="border: none;"></td>';
+
+		// green for on, white for off
 		var state = "off";
 		var color = "white";
-		if (states[i]) {
+		if (stateCombination[i]) {
 			state = "on";
 			color = "#10d010";
 		}
@@ -55,8 +69,11 @@ appendSteadyStatesTable = function(states) {
 updateSteadyStates = function() {
 	var currentStateCombination = [];
 	var i;
-	for (i in SteadyStatesNodes) {
-		currentStateCombination.push( network.state[SteadyStatesNodes[i]] );
+	for (i in networkInputNodes) {
+		currentStateCombination.push( network.state[networkInputNodes[i]] );
+	}
+	for (i in networkOutputNodes) {
+		currentStateCombination.push( network.state[networkOutputNodes[i]] );
 	}
 	if (encounteredStateCombinations.indexOf(currentStateCombination) < 0) {
 		appendSteadyStatesTable(currentStateCombination);
